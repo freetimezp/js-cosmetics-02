92d0b2a9003f03c4ab275d945192532124c8e55a
freetimezp <kraftukrnet@ukr.net>
2023-04-15 18:30:27 +0300

    09. js-cosmetics-02. Animation.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,9 +4,13 @@ import HeroImg from '../../assets/hero.png';
 import { RiShoppingBagFill } from 'react-icons/ri';
 import { BsArrowRight } from 'react-icons/bs';
 
+import { motion } from 'framer-motion';
+
 import css from './Hero.module.css';
 
 const Hero = () => {
+    const transition = { duration: 3, type: 'spring' };
+
     return (
         <div className={css.container}>
             <div className={css.h_sides}>
@@ -20,9 +24,28 @@ const Hero = () => {
             </div>
 
             <div className={css.wrapper}>
-                <div className={css.blueCircle}></div>
-                <img src={HeroImg} alt="hero" width={600} />
-                <div className={css.cart2}>
+                <motion.div
+                    initial={{ bottom: '2rem' }}
+                    whileInView={{ bottom: '0rem' }}
+                    transition={transition}
+                    className={css.blueCircle}
+                ></motion.div>
+
+                <motion.img 
+                    src={HeroImg} 
+                    alt="hero" 
+                    width={600}
+                    initial={{ bottom: '-2rem' }}
+                    whileInView={{ bottom: '0rem' }}
+                    transition={transition}
+                />
+
+                <motion.div 
+                    className={css.cart2}
+                    initial={{ right: "4%" }}
+                    whileInView={{ right: "2%" }}
+                    transition={transition}
+                >
                     <RiShoppingBagFill />
                     <div className={css.signup}>
                         <span>Best Signup Offers</span>
@@ -30,7 +53,7 @@ const Hero = () => {
                             <BsArrowRight />
                         </div>
                     </div>
-                </div>
+                </motion.div>
             </div>
 
             <div className={css.h_sides}>
